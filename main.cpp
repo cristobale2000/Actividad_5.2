9f215a51b733d8f94f6a800c6c95722f3a8a11d5
Cristóbal Escamilla <cristobalescamilla2000@gmail.com>
2020-11-24 21:44:05 -0600

    Initial files

---
+++ main.cpp
@@ -0,0 +1,27 @@
+/*
+ * Programación de estructuras de datos y algoritmos fundamentales
+ * Actividad 5.2 - Actividad integral sobre uso de códigos Hash
+ * 
+ * Equipo 13
+ * 
+ * Fernando Doddoli Lankenau - A00827038
+ * Mauricio Eugenio Zambrano Díaz - A00827055
+ * Cristóbal Alberto Escamilla Sada - A00827074
+ * 
+ * 28 de noviembre del 2020
+ */
+
+#include<fstream>
+#include<iostream>
+#include<string>
+#include<vector>
+
+using namespace std;
+
+
+int main(){
+
+
+
+    return 0;
+}
